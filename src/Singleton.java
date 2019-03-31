de415b3f822459613aefc330fe093af82b0783ab
Chelu <joseluism@faculty.ie.edu>
2019-03-31 20:49:45 +0200

    Initial

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,20 +1,25 @@
+// Source code: https://refactoring.guru/design-patterns/singleton/java/example#example-2
 public final class Singleton {
-    private static Singleton instance;
+    // The field must be declared volatile so that double check lock would work.
+    //
+    // See: https://refactoring.guru/java-dcl-issue
+    private static volatile Singleton instance;
+
     public String value;
 
     private Singleton(String value) {
-        // Following code emulates slow initialization.
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
         this.value = value;
     }
 
     public static Singleton getInstance(String value) {
-        if (instance == null) {
-            instance = new Singleton(value);
+        Singleton result = instance;
+        if (result == null) {
+            synchronized (Singleton.class) {
+                result = instance;
+                if (result == null) {
+                    instance = result = new Singleton(value);
+                }
+            }
         }
         return instance;
     }
